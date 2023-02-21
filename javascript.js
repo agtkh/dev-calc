364a924b5f3c51a055f41132325889dccaca27db
agtkh <108037747+agtkh@users.noreply.github.com>
2023-02-21 13:14:35 +0900

    関数に説明コメントを追加

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 function calc_formula(formula_str) {
     /*
-    
+    現在の式を評価し、結果を返す。
+    式が不正の場合はnullを返す。
     */
     let re;
     try {
@@ -26,15 +27,24 @@ function is_op() {
     /**
      * これは計算記号か
      */
+    return !(this.is_num(char));
 }
 
 
 class Formula {
+    /**
+     * 計算式管理用のクラス
+     */
     constructor() {
         this.base = 10;
         this.formula_list = [];
     }
     display_num() {
+        /**
+         * 結果画面に教示する数字を返す。
+         * 数字入力中はその数字。
+         * = の後は計算結果など
+         */
         let last_key = this.formula_list[this.formula_list.length - 1];
         if (typeof last_key == 'number') {
             // 数字だった場合
@@ -48,6 +58,10 @@ class Formula {
         return 0;
     }
     push(key) {
+        /**
+         * 計算式に追加(入力)
+         * 数値の入力と記号の入力を適切に追加する。
+         */
         if (this.formula_list.length == 0) {
             this.formula_list.push(key);
         } else {
@@ -55,10 +69,12 @@ class Formula {
             if (typeof key == 'number' && typeof last_key == 'number') {
                 this.formula_list[this.formula_list.length - 1] = last_key * this.base + key;
             } else if (typeof key == 'string' && typeof last_key == 'string') {
-                // 上書き
+                // 連続の記号入力
                 if (last_key == '=') {
+                    // 結果を計算して計算式を更新
                     this.formula_list= [calc_formula(this.to_str().slice(0, -1)), key];
                 } else {
+                    // 上書き
                     this.formula_list[this.formula_list.length - 1] = key;
                 }
             } else {
@@ -67,23 +83,31 @@ class Formula {
         }
     }
     to_str() {
+        /**
+         * 数式を文字列化して返す。
+         */
         return this.formula_list.join('');
     }
     change_base(base) {
+        /**
+         * 入力のベース(進数)を変更する。
+         */
         this.base = base;
         let last_key = this.formula_list[this.formula_list.length - 1];
         if (typeof last_key == "number") {
+            // 入力途中の場合、入力中の数字を消す。
             this.formula_list.pop();
         }
     }
     clear() {
+        // 計算式のクリア
         this.formula_list = [];
     }
 }
 
 function gen_bin_html(val) {
     /*
-
+    2進数表示用のhtmlを返す
     */
     let bin_str = '';
     for (let i = 0; i < 64; i++) {
@@ -103,7 +127,7 @@ function gen_bin_html(val) {
 }
 function gen_hex_html(val) {
     /*
-    
+    16進数表示用のhtmlを返す
     */
     const hex_dict = "0123456789ABCDEF";
     let hex_str = '';
@@ -116,6 +140,9 @@ function gen_hex_html(val) {
 }
 
 function update(formula) {
+    /**
+     * 表示の更新
+     */
     let display_num = formula.display_num();
     let formula_str = formula.to_str();
     $('#dec_result_field').html(display_num);
@@ -165,10 +192,10 @@ $(function () {
         update(current_formula);
     });
 
-    // $('#btn_del').on('click', function () {
-    //     current_formula = current_formula.slice(0, -1);
-    //     update(current_formula);
-    // });
+    $('#btn_del').on('click', function () {
+        // To-Do
+        update(current_formula);
+    });
 
     $('.num_btn').on('click', function () {
         console.log('num btn pushed0');
