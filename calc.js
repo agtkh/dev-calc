6f757ef5f28ddb589adb6b5e01a0473a81484ebc
agtkh <108037747+agtkh@users.noreply.github.com>
2023-02-21 13:24:34 +0900

    jsファイルをディレクトリでまとめた

--- calc.js
+++
@@ -1,7 +0,0 @@
-function calculate(formula_str) {
-    console.log( formula_str );
-    for (const match of formula_str.matchAll(/\((.*)\)/g)) {
-        console.log(match[1]);
-    }
-}
-calculate('a + c * (e + f) + (g + h) + d + b');
